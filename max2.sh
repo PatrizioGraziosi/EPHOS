0e77984a55ff95eb7fd2aaa063c86890ad700bf2
Patrizio Graziosi <tizio82@gmail.com>
2025-05-11 09:01:16 +0200

    Create max2.sh

---
+++ max2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# find the highest absolute value of the stress on atoms
+# you must enetr the path of the OUTCAR file, the nuber of atoms, and the file name of the temp file you want to use
+
+path_to_file='chiralDNTT/OUTCAR'
+filename=temp2
+n_atoms=192
+
+grep 'total drift' -B $(( $n_atoms+3 )) $path_to_file  | tail -n $(( $n_atoms+2 ))  > temp
+
+
+awk 'NR < "'"$n_atoms"'" {print $4; $5; $6} ' temp > $filename
+
+
+
+
+max=0
+
+for line in $(cat $filename) ;  do
+
+temp_value="$line"
+
+abs_temp_value=${temp_value#-}
+
+
+if  (( $( echo "$abs_temp_value > $max" | bc -l  ) ))  ; then
+max=$abs_temp_value
+fi
+
+done
